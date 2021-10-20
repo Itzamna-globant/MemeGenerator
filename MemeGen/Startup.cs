7469269de68b7663c101ff1d96d7c0630f4d3dc6
Itzamna "Dradel" Zuñiga <Itzamna.zuniga@globant.com>
2021-10-20 17:58:51 -0500

    [feat] Configure sql server as dbcontext provider

--- MemeGen/Startup.cs
+++ MemeGen/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+
+using MemeGen.Infrastructure.Persistence;
 
 namespace MemeGen
 {
@@ -19,12 +22,17 @@ namespace MemeGen
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MemeGen", Version = "v1" });
             });
+
+            services.AddDbContext<MemeDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("MemeConnection"), 
+                    m => m.UseNetTopologySuite())
+                );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
