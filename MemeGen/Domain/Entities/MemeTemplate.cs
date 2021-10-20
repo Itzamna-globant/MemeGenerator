c510e44c76cb53cd1b662a8f46df25d6ac70ddbd
Itzamna "Dradel" Zuñiga <Itzamna.zuniga@globant.com>
2021-10-20 17:14:56 -0500

    [feat] Add MemeTemplate

---
+++ MemeGen/Domain/Entities/MemeTemplate.cs
@@ -0,0 +1,13 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace MemeGen.Domain.Entities
+{
+    public class MemeTemplate
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Polygon TextRectangleLocation { get; set; }
+    }
+}
